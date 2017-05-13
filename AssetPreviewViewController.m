a930b5579db64212d9b1595aaaaa8491df78acc5
Brian <brwong20@g.ucla.edu>
2017-05-12 22:34:47 -0700

    Fixed hold to preview cancel frame animation

--- AssetPreviewViewController.m
+++ AssetPreviewViewController.m
@@ -59,6 +59,10 @@
     [self.imageView setUserInteractionEnabled:YES];
     [self.imageView setClipsToBounds:YES];
     
+    //Load the lower res image first while waiting for full res to load. Set the frame to match the image as well so it's not fullscreen in case user cancels loading.
+    [self.imageView setImage:self.placeHolderImg];
+    [self resizeImageFrame];
+    
     [self.view addSubview:self.imageView];
     
 #warning USE THIS LOGIC TO GET SMALLER URLS FOR SMALLER DEVICES
@@ -94,54 +98,23 @@
 
 
 - (void)loadImageFromURL{
-    [self.imageView setImage:self.placeHolderImg];//Load lower res image first while waiting for higher one to download
-    
     [self.imageView setPin_updateWithProgress:YES];
     [self.imageView pin_setImageFromURL:self.currentImgURL completion:^(PINRemoteImageManagerResult * _Nonnull result) {
-        
-        //Resize frame based on downloaded image's aspect ratio
-        float widthRatio = _imageView.bounds.size.width / _imageView.image.size.width;
-        float heightRatio = _imageView.bounds.size.height / _imageView.image.size.height;
-        float scale = MIN(widthRatio, heightRatio);
-        float imageWidth = scale * _imageView.image.size.width;
-        float imageHeight = scale * _imageView.image.size.height;
-        
-        self.imageView.frame = CGRectMake(APPLICATION_FRAME.size.width/2 - imageWidth/2, APPLICATION_FRAME.size.height/2 - imageHeight/2, imageWidth, imageHeight);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self resizeImageFrame];
+        });
     }];
 }
 
--(void)displayImage:(UIImage*)assetImage
-{
-    //Remove stale imageView if needed
-//    if (_imageView && self.imageView.window) {
-//        [self.imageView removeFromSuperview];
-//    }
+//Resize frame based on image's aspect ratio (for animation purposes)
+- (void)resizeImageFrame{
+    float widthRatio = _imageView.bounds.size.width / _imageView.image.size.width;
+    float heightRatio = _imageView.bounds.size.height / _imageView.image.size.height;
+    float scale = MIN(widthRatio, heightRatio);
+    float imageWidth = scale * _imageView.image.size.width;
+    float imageHeight = scale * _imageView.image.size.height;
     
-    //Setup frame for Orientation and aspect ration
-    //CGRect imageFrame = AVMakeRectWithAspectRatioInsideRect(assetImage.size, APPLICATION_FRAME);
-    
-//    if (assetImage.size.width > assetImage.size.height) {
-//        self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-//        self.imageView.frame = CGRectMake(0, 0, CGRectGetHeight(APPLICATION_FRAME), CGRectGetWidth(APPLICATION_FRAME));
-//        self.view.frame = CGRectMake(0, 0, CGRectGetHeight(APPLICATION_FRAME), CGRectGetWidth(APPLICATION_FRAME));
-//        
-//        self.view.transform = CGAffineTransformMakeRotation(M_PI_2);
-//        self.view.center = CGPointMake(CGRectGetWidth(APPLICATION_FRAME)/2, CGRectGetHeight(APPLICATION_FRAME)/2);
-//        [self.imageView setCenter:CGPointMake(CGRectGetHeight(APPLICATION_FRAME)/2, CGRectGetWidth(APPLICATION_FRAME)/2)];
-//    }
-//    else
-//    {
-//        [self.imageView setCenter:CGPointMake(CGRectGetMidX(APPLICATION_FRAME), CGRectGetMidY(APPLICATION_FRAME))];
-//    }
-    
-    [self.imageView setCenter:CGPointMake(CGRectGetMidX(APPLICATION_FRAME), CGRectGetMidY(APPLICATION_FRAME))];
-    
-    [self.imageView setImage:assetImage];
-    
-    //Tag cannot be Zero!!!
-    [self.imageView setTag:[self.currentAsset hash]];
-    
-    //[self.view addSubview:self.imageView];
+    self.imageView.frame = CGRectMake(APPLICATION_FRAME.size.width/2 - imageWidth/2, APPLICATION_FRAME.size.height/2 - imageHeight/2, imageWidth, imageHeight);
 }
 
 - (BOOL)prefersStatusBarHidden{
